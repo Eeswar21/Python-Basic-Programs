96bfd264fc2d4821a98dce45f8edee5dbff45071
Eswar <48028316+Eeswar21@users.noreply.github.com>
2020-02-20 14:24:05 +0000

    Create matrix_add.py
    Add two matrices using list comprehension

---
+++ matrix_add.py
@@ -0,0 +1,19 @@
+# Program to add two matrices using list comprehension 
+   
+X = [[1,2,3], 
+    [4 ,5,6], 
+    [7 ,8,9]] 
+   
+Y = [[9,8,7], 
+    [6,5,4], 
+    [3,2,1]] 
+  
+result = [[X[i][j] + Y[i][j]  for j in range
+(len(X[0]))] for i in range(len(X))] 
+   
+for r in result: 
+    print(r)
+
+
+# Program to add two matrices using zip() 
+# result = [map(sum, zip(*t)) for t in zip(X, Y)] 
